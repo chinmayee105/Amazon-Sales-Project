06efe3895281b8feb3a60c39753807939c915de3
chinmayee105 <147787799+chinmayee105@users.noreply.github.com>
2024-07-20 17:53:01 +0530

    Amazon Sales .py

--- Amazon Sales .py
+++ Amazon Sales .py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
 
 import pandas as pd
 import numpy as np
@@ -10,157 +5,74 @@ import matplotlib.pyplot as plt
 get_ipython().run_line_magic('matplotlib', 'inline')
 import seaborn as sns
 
-
-# In[2]:
-
-
 df = pd.read_csv('Amazon Sale Report.csv')
-
-
-# In[3]:
-
 
 df.shape
 
-
-# In[4]:
-
-
 df.head(5)
-
-
-# In[78]:
-
 
 df.tail(5)
 
-
-# In[79]:
-
-
 df.info()
-
-
-# In[80]:
 
 
 # Block Unrelated/Blank Columns
 
 df.drop(['New','PendingS'], axis=1, inplace=True)
 
-
-# In[81]:
-
-
 df.info()
 
-
-# In[82]:
-
-
+---------------------------------------------------------------------------------------------------------------------------------------------------
 # Check the Null values
 
 pd.isnull(df)
 
-
-# In[83]:
-
+---------------------------------------------------------------------------------------------------------------------------------------------------
 
 # Sum wil give total values of null values
 
 pd.isnull(df).sum()
 
-
-# In[84]:
-
-
 df.shape
-
-
-# In[85]:
-
 
 df.columns
 
-
-# In[86]:
-
-
+----------------------------------------------------------------------------------------------------------------------------------------------------
 # Change Data Type
 df[ 'ship-postal-code' ]=df[ 'ship-postal-code' ].astype('float')
 
-
-# In[87]:
-
-
 df['ship-postal-code'].dtype
-
-
-# In[88]:
-
 
 df['Date']=pd.to_datetime(df[ 'Date' ])
 
-
-# In[89]:
-
-
 df.info()
-
-
-# In[90]:
-
 
 df.columns
 
-
-# In[91]:
-
-
+----------------------------------------------------------------------------------------------------------------------------------------------------
 # Rename Columns
 df.rename(columns={'Qty':'Quantity'})
 
-
-# In[92]:
-
-
 df.describe(include='object')
 
-
-# In[93]:
-
-
+----------------------------------------------------------------------------------------------------------------------------------------------------
 # Use describe() for specific columns
 df[['Qty','Amount']].describe()
 
+----------------------------------------------------------------------------------------------------------------------------------------------------
 
-# ## Exploratory Data Analysis
-
-# In[94]:
-
-
+# Exploratory Data Analysis
 df.columns
 
-
-# ### * Size
-
-# In[95]:
-
-
+*** Size :
 ax = sns.countplot(x='Size',data=df)
 
+*** Group By :
 
-# ### * Group By
-
-# #### The GroupBy() function in pandas is used to group data based on 1 or more columns in a Dataframe
-
-# In[98]:
+# The GroupBy() function in pandas is used to group data based on 1 or more columns in a Dataframe.
 
 
 df.groupby(['Size'], as_index=False)['Qty'].sum().sort_values(by='Qty',ascending=False)
-
-
-# In[99]:
 
 
 S_Qty=df.groupby(['Size'], as_index=False)['Qty'].sum().sort_values(by='Qty',ascending=False)
@@ -168,36 +80,23 @@ S_Qty=df.groupby(['Size'], as_index=False)['Qty'].sum().sort_values(by='Qty',asc
 sns.barplot(x='Size',y='Qty',data=S_Qty)
 
 
-# ###### From above Graph you can see that most of the Qty buys M-size in the sales
+# From above Graph you can see that most of the Qty buys M-size in the sales.
 
-# ### * Courier Status
-
-# In[100]:
-
+*** Courier Status :
 
 sns.countplot(data=df, x='Courier Status', hue= 'Status')
-
-
-# In[101]:
-
 
 plt.figure(figsize=(10,5))
 ax = sns.countplot(data=df, x='Courier Status', hue='Status')
 plt.show()
 
 
-# ###### From Above Graph the majorityof the orders are shipped through the courier
+# From Above Graph the majorityof the orders are shipped through the courier.
 
-# In[102]:
-
-
+---------------------------------------------------------------------------------------------------------------------------------------------------
 # Histogram
 
 df['Size'].hist()
-
-
-# In[103]:
-
 
 df['Category'] = df['Category'].astype(str)
 column_data = df['Category']
@@ -207,10 +106,8 @@ plt.xticks(rotation=90)
 plt.show()
 
 
-# ###### From above Graph you can see that most of the buyers are T-Shirt
-
-# In[107]:
-
+# From above Graph you can see that most of the buyers are T-Shirt.
+-----------------------------------------------------------------------------------------------------------------------------------------------
 
 # Checking B2B Data by using pie chart
 
@@ -225,14 +122,13 @@ plt.pie(B2B_Check, labels=B2B_Check.index, autopct = '%1.1f%%')
 plt.show()
 
 
-# ###### From above chart we can see that maximum i.e. 99.3% of buyers are retailers and 0.7% are B2B buyers
+# From above chart we can see that maximum i.e. 99.3% of buyers are retailers and 0.7% are B2B buyers.
 
-# ### Category and Size the products
+------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-# In[109]:
+# Category and Size the products. 
 
-
-# Prepare data for Scatter Plot
+# Prepare data for Scatter Plot :
 x_data = df['Category']
 y_data = df['Size']
 
@@ -244,11 +140,8 @@ plt.title('Scatter Plot')
 plt.xticks(rotation=90)
 plt.show()
 
-
-# ### Which city or state buy the most of the products
-
-# In[110]:
-
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Which city or state buy the most of the products.
 
 plt.figure(figsize=(12,6))
 sns.countplot(data=df, x='ship-state')
@@ -258,14 +151,12 @@ plt.title('Distribution of State')
 plt.xticks(rotation=90)
 plt.show()
 
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-# In[115]:
-
-
-# Top 10_states
+# Top 10_states :
 top_10_state = df['ship-state'].value_counts().head(10)
 
-# Plot count of cities by state
+# Plot count of cities by state :
 
 plt.figure(figsize=(12,6))
 sns.countplot(data=df[df['ship-state'].isin(top_10_state.index)], x='ship-state')
@@ -276,14 +167,8 @@ plt.xticks(rotation=45)
 plt.show()
 
 
-# ###### From above Graph you can see that most of the buyers are Maharastra State
+# From above Graph you can see that most of the buyers are Maharastra State.
 
-# ## -- Conclusion
+# -- Conclusion :
 
-# ###### The Data Analysis reveals that the business has a significant customers base in Maharastra State, mainly serves retailers, orders through Amazon, experiences high demand for T-shirt, and sees M-Size as the preferred choice among buyers.
-
-# In[ ]:
-
-
-
-
+# The Data Analysis reveals that the business has a significant customers base in Maharastra State, mainly serves retailers, orders through Amazon, experiences high demand for T-shirt, and sees M-Size as the preferred choice among buyers.
